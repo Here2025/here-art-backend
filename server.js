ffcf3f595b3c976e2fde8a9c2d2e9eb34f6966c4
Here2025 <zmulwanda@gmail.com>
2026-06-01 11:50:03 -0400

    Remove public HERE demo seed route after seeding

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { Pool } = require('pg');
 const { registerPlatformRoutes } = require('./platformRoutes');
-const { registerDemoSeedRoutes } = require('./demoSeedRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -348,7 +347,6 @@ app.post(['/api/artists', '/artists'], async (req, res, next) => {
 });
 
 registerPlatformRoutes(app, pool);
-registerDemoSeedRoutes(app, pool);
 
 app.use((error, req, res, next) => {
   console.error(error);
